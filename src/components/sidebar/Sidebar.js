67e37138a33d64e1c81ced23e87649090daa244a
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-25 10:45:04 +0700

    add sidebar mapping logic

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,15 @@
 import React from "react";
+import sidebar from "../../data/sidebar";
+import SidebarMapping from "./SidebarMapping";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="grow max-w-xs border border-primary">
+      <div className="h-full xl:h-[92vh] flex">
+        <div className="sidebar mx-4 bg-transparent text-mediumfont w-64 space-y-6 py-2 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
+          <SidebarMapping data={sidebar} />
+        </div>
+      </div>
+    </div>
+  );
 }
