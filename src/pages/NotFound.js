64864fad9311ef5110624f1dd7d1227667728117
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-25 11:02:34 +0700

    redirect to dashboard on 404 not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
+  let navigate = useNavigate();
+  useEffect(() => {
+    navigate("/dashboard", { replace: true });
+  }, [navigate]);
   return <div>NotFound</div>;
 }
