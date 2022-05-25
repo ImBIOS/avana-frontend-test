d663244f7d0716b991d30296f8e6870b08d7284e
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-25 07:51:17 +0700

    add toggle logic to match the data use case

--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -1,6 +1,22 @@
 import { useSelector } from "react-redux";
+import Restricted from "../restricted/Restricted";
+
+const search = (key, inputArray) => {
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].id === key) {
+      return inputArray[i];
+    }
+  }
+};
 
 export default function Toggle({ id, children }) {
-  const isShow = useSelector((state) => state.toggle[id]) ?? false;
-  return isShow ? children : null;
+  const toggle = useSelector((state) => state.toggles) ?? false;
+  const isShowed = search(id, toggle)?.isShowed;
+  const isAllowed = search(id, toggle)?.isAllowed;
+
+  if (isShowed && isAllowed) {
+    return children;
+  } else if (isShowed && !isAllowed) {
+    return <Restricted>{children}</Restricted>;
+  } else return null;
 }
