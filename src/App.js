a7b7aec05084736af62c029bc9f34915520a060e
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-25 11:01:49 +0700

    replace routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
-import MainView from "./components/main-view/MainView";
 import DashboardLayout from "./layout/DashboardLayout";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
 
 function App() {
   return (
     <DashboardLayout>
       <Sidebar />
-      <MainView />
+      <div className="grow text-center border border-white bg-lightgrey">
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route exact path="/" element={<Dashboard />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
     </DashboardLayout>
   );
 }
