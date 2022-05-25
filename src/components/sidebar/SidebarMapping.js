7f3e22bb0635ae07a0ff73e5a11db5eb231e3e61
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-25 20:37:00 +0700

    add Toggle to sidebar

--- src/components/sidebar/SidebarMapping.js
+++ src/components/sidebar/SidebarMapping.js
@@ -1,57 +1,63 @@
 import React from "react";
-import { FaChevronDown } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+import Toggle from "../toggle/Toggle";
 
 export default function SidebarMapping({ data }) {
+  const location = useLocation();
   const [isOpen, setIsOpen] = React.useState({});
 
-  const pathname = window.location.pathname || "/";
+  const pathname = location.pathname;
 
   const handleMenuClick = (id) => setIsOpen({ [id]: !isOpen[id] });
 
   return (
     <nav>
       {data.map((item) => {
-        if (item.isShowed)
-          return (
-            <div key={item.id}>
+        return (
+          <div key={item.id}>
+            <Toggle id={item.id} toggle={item}>
               <Link to={!item.childs ? item.id : "#"}>
                 <div
                   onClick={item.childs && (() => handleMenuClick(item.id))}
                   key={item.id}
-                  className={`flex justify-between items-center my-4 py-2.5 px-4 rounded transition duration-200 hover:bg-blue-500 hover:text-white ${
-                    pathname.startsWith(item.id) && "bg-blue-500"
-                  } ${item.childs && "bg-lightbg-secondary my-0 font-bold"}`}
+                  className={`flex justify-between items-center my-4 p-4 transition duration-200 hover:bg-lightgrey hover:text-white ${
+                    pathname.startsWith(`/${item.id}`) &&
+                    "bg-lightgrey border-r-2 border-primary"
+                  } ${item.childs && "bg-lightbg-secondary my-0"}`}
                 >
                   <span className="align-middle ml-2">{item.title}</span>
-                  {item.childs && <FaChevronDown />}
+                  {item.childs && !isOpen[item.id] && <FaChevronDown />}
+                  {item.childs && isOpen[item.id] && <FaChevronUp />}
                 </div>
               </Link>
+            </Toggle>
 
-              {item.childs && (
-                <div
-                  className="rounded p-2 max-h-[50vh] overflow-y-auto"
-                  hidden={!isOpen[item.id]}
-                >
-                  {item.childs.map((child) => (
+            {item.childs && (
+              <div
+                className="p-2 max-h-[30vh] overflow-y-auto"
+                hidden={!isOpen[item.id]}
+              >
+                {item.childs.map((child) => (
+                  <Toggle id={child.id} toggle={child}>
                     <a
                       key={child.id}
                       href={child.id}
-                      className={`flex justify-between items-center py-2.5 my-4 px-4 rounded transition duration-200 hover:bg-blue-500 hover:text-white ${
-                        pathname.startsWith(child.id) &&
-                        "bg-blue-500 text-white font-bold"
+                      className={`flex justify-between items-center py-2.5 my-4 px-4 border-b opacity-50 transition duration-200 hover:bg-lightgrey hover:text-white ${
+                        pathname.startsWith(`/${child.id}`) &&
+                        "bg-lightgrey text-white font-bold"
                       }`}
                     >
-                      <span className="align-middle ml-2">{child.title}</span>
+                      <span className="align-middle ml-2 text-mediumfont">
+                        {child.title}
+                      </span>
                     </a>
-                  ))}
-                </div>
-              )}
-            </div>
-          );
-
-        // return null if item is not showed
-        return null;
+                  </Toggle>
+                ))}
+              </div>
+            )}
+          </div>
+        );
       })}
     </nav>
   );
