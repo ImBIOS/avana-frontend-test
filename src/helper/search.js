0560a7931dba953ebd9dee931451d4e29e0298c0
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-25 20:40:52 +0700

    remove unused code: simple search

--- src/helper/search.js
+++
@@ -1,9 +0,0 @@
-const search = (key, inputArray) => {
-  for (let i = 0; i < inputArray.length; i++) {
-    if (inputArray[i].id === key) {
-      return inputArray[i];
-    }
-  }
-};
-
-export default search;
