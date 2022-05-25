46caecc07161eef16907dd824a004e83cb10a144
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-25 08:17:03 +0700

    setup DashboardLayout

--- src/layout/DashboardLayout.js
+++ src/layout/DashboardLayout.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function DashboardLayout({ children }) {
-  return <div>{children}</div>;
+  return (
+    <div className="bg-darkgrey text-brightfont w-full h-screen">
+      <main className="flex flex-row mx-auto max-w-7xl">{children}</main>
+    </div>
+  );
 }
