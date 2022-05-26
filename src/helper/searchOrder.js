d07c7c8258f1cd129aa02430e453723625f1468b
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-26 12:46:39 +0700

    remove searchOrder.js

--- src/helper/searchOrder.js
+++
@@ -1,9 +0,0 @@
-const searchOrder = (key, inputArray) => {
-  for (let i = 0; i < inputArray.length; i++) {
-    if (inputArray[i].id === key) {
-      return i;
-    }
-  }
-};
-
-export default searchOrder;
