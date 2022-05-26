ef17e9693910991c194331afec02b9da1fcf4cbd
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-26 08:41:13 +0700

    add all product page

---
+++ src/pages/AllProduct.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AllProduct() {
+  return (
+    <>
+      <main>
+        <h2>All Product</h2>
+        <p>You will see all product list in this page.</p>
+      </main>
+    </>
+  );
+}
